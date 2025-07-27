a8e278447e06175f725d2b27ebc45de34f5c9a4e
JP <jyotheendra@puntechsolutions.com>
2025-07-27 16:35:40 +1000

    added new instruments

--- config.py
+++ config.py
@@ -2,7 +2,35 @@
 SYMBOLS = {
     "BTC-USD": "BTC-USD",
     "AUD/USD": "AUDUSD=X",
-    "USD/JPY": "USDJPY=X"
+    "AUD/JPY": "AUDJPY=X",
+    "AUD/NZD": "AUDNZD=X",
+    "AUD/CHF": "AUDCHF=X",
+    "AUD/CAD": "AUDCAD=X",
+    "EUR/USD": "EURUSD=X",
+    "USD/JPY": "USDJPY=X",
+    "GBP/USD": "GBPUSD=X",
+    "USD/CAD": "USDCAD=X",
+    "USD/CHF": "USDCHF=X",
+    "NZD/USD": "NZDUSD=X",
+    "EUR/GBP": "EURGBP=X",
+    "EUR/JPY": "EURJPY=X",
+    "GBP/JPY": "GBPJPY=X",
+    "EUR/CHF": "EURCHF=X",
+    "CAD/JPY": "CADJPY=X",
+    "EUR/CAD": "EURCAD=X",
+    "EUR/NZD": "EURNZD=X",
+    "EUR/AUD": "EURAUD=X",
+    "GBP/AUD": "GBPAUD=X",
+    "GBP/CAD": "GBPCAD=X",
+    "GBP/NZD": "GBPNZD=X",
+    "CAD/CHF": "CADCHF=X",
+    "NZD/CHF": "NZDCHF=X",
+    "NZD/JPY": "NZDJPY=X",
+    "USD/SEK": "USDSEK=X",
+    "USD/NOK": "USDNOK=X",
+    "USD/DKK": "USDDKK=X",
 }
+
+
 REFRESH_INTERVAL = 15  # seconds
 HOURLY_REFRESH_INTERVAL = 3600  # seconds
